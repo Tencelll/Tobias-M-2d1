566786e486d9459a01997d6d8dbb6ec3c9b1ac3e
Tencelll <68108615+Tencelll@users.noreply.github.com>
2021-08-15 15:36:30 +0200

    Update Svar-01.java

--- Svar-01.java
+++ Svar-01.java
@@ -1,7 +1,9 @@
 class Simple{  
     public static void main(String args[]){  
         System.out.println( 0.2 == 0.1 + 0.1 );
+	// true
 	System.out.println( 0.3 == 0.1 + 0.1 + 0.1 );
+	// false
         // Floating point tal repræsenteres ofte ikke præcist og dermed kan de ikke altid bruges med 100% nøjagtighed.
     }  
 }
